a48f1d983f2423adfbca97186478ed9208120d2f
VladimirKalatsei <vovkqh@gmail.com>
2025-05-05 16:22:26 +0300

    Lab 7

--- src/main/java/com/example/lab001/Lab001Application.java
+++ src/main/java/com/example/lab001/Lab001Application.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab001Application
-{
-
+public class Lab001Application {
     public static void main(String[] args) {
         SpringApplication.run(Lab001Application.class, args);
     }
